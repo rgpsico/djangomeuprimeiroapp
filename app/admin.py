0956cb40faba1f257b7ed0541cfae5dc06fcc18e
rgpsico <gerro121@hotmail.com>
2021-07-12 01:31:01 -0300

    aaa

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Categoria
+from .models import Produtos
+
+
+admin.site.register(Categoria)
+admin.site.register(Produtos)
