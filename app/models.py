c3621676905bbafcc0998e11d1c8fa81aa9d49a4
rgpsico <gerro121@hotmail.com>
2021-07-12 01:45:23 -0300

    aaa

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class venda(models.Model):
 
 class Pedidos(models.Model):
     item = models.CharField(max_length=100, null=True, blank=True)
-    valor = models.IntegerField()
+    valor = models.IntegerField(null=True)
     quantidade = models.CharField(max_length=100, null=True, blank=True)
     usuario = models.CharField(max_length=100, null=True, blank=True)
 
